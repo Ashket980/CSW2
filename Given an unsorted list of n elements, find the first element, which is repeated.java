6ccde3e423706c8d55a7f1c40f6b1ebd50407d24
Ashish Kumar <85364791+Ashket980@users.noreply.github.com>
2022-06-27 20:09:40 +0530

    Create Given an unsorted list of n elements, find the first element, which is repeated.java

---
+++ Given an unsorted list of n elements, find the first element, which is repeated.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+
+public class Q1 
+{
+	public static void main(String[] args) 
+	{
+		int a[]= {1,7,4,2,7,1,8,9,1};
+		
+		HashSet<Integer> hs=new HashSet<Integer>();
+				
+		for(int i=0;i<a.length;i++)
+		{
+			if(!hs.contains(a[i]))
+			{
+				hs.add(a[i]);
+			}
+			
+			else
+			{
+				System.out.println(a[i]);
+				break;
+			}
+		}
+
+	}
+
+}
