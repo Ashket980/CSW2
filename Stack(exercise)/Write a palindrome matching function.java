b8736e8beaf336ae6082f890bff56460e216e0b7
Ashish Kumar <85364791+Ashket980@users.noreply.github.com>
2022-07-04 23:17:30 +0530

    Create Write a palindrome matching function.java

---
+++ Stack(exercise)/Write a palindrome matching function.java
@@ -0,0 +1,44 @@
+public class Q6
+{
+    static boolean sentencePalindrome(String str)
+    {   
+        int l = 0;
+        int h = str.length()-1;
+         
+        str = str.toLowerCase();
+         
+        while(l <= h)
+        {
+             
+            char getAtl = str.charAt(l);
+            char getAth = str.charAt(h);
+             
+            if (!(getAtl >= 'a' && getAtl <= 'z'))
+                l++;
+             
+            else if(!(getAth >= 'a' && getAth <= 'z'))
+                h--;
+             
+            else if( getAtl == getAth)
+            {
+                l++;
+                h--;
+            }
+             
+            else
+                return false;
+        }
+         
+        return true;   
+    }
+     
+    public static void main(String[] args)
+    {
+        String str = "Too hot to hoot.";
+        if( sentencePalindrome(str))
+          System.out.println("Sentence is palindrome");
+        else
+          System.out.println("Sentence is not" + " " +
+                                         "palindrome");
+    }
+}
