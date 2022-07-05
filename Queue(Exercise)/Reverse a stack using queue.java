46bad71ccdc7d7e7626f8d51da9b3b0abb2616d4
Ashish Kumar <85364791+Ashket980@users.noreply.github.com>
2022-07-05 21:40:04 +0530

    Update Reverse a stack using queue.java

--- Queue(Exercise)/Reverse a stack using queue.java
+++ Queue(Exercise)/Reverse a stack using queue.java
@@ -1,1 +1,24 @@
 
+import java.util.*;
+public class sttttttt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack<Integer> ss=new Stack<>();
+		ss.add(1);
+		ss.add(2);
+		ss.add(3);
+		ss.add(4);
+		ss.add(5);
+		ArrayDeque <Integer>qq=new ArrayDeque<>();
+		System.out.println(ss.peek());
+		System.out.println(ss);
+		while(!(ss.isEmpty()))
+			qq.add(ss.pop());
+		while(!(qq.isEmpty()))
+			ss.push(qq.remove());
+		System.out.println(ss);
+		
+	}
+
+}
